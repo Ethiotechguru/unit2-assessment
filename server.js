0555fc55963eebe6d13afbff78cb6e56ee5ec114
Samuel Belete <samuelbelete@Samuels-MacBook-Pro.local>
2019-07-09 16:25:34 -0700

    unit2-assessment

--- server.js
+++ server.js
@@ -1,16 +1,41 @@
 const express = require('express');
 const methodOverride = require('method-override');
-
+const db = require('./models');
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: false}));
 app.use(express.static('static'));
 app.use(methodOverride('_method'));
-
+// app.use('/static', express.static(path.join(__dirname, 'css')));
 // WRITE YOUR ROUTES HERE /////////////////////
+app.get('/', function(req, res){
+    db.widget.findAll().then(function(widgets){
+        res.render('index', {widgets:widgets});
+    })
+})
+app.post('/', function(req, res){
+    db.widget.create({
+        discription: req.body.description,
+        quantity: parseInt(req.body.quantity),
+    }).then(function(widget){
+        res.redirect('/');
+    })
+})
+app.delete('/:id', function(req, res) {
+      db.widget.destroy({
+          where: {
+              id:req.params.id,
+          }
+        }).then(function(pics) {
+            res.redirect('/');
+        })
+    })
 
 
+app.get('/', function(req, res){
+    res.render('index')
+})
 
 // YOUR ROUTES ABOVE THIS COMMENT /////////////
 
